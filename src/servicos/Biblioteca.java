4b6a6456b87e97f60193c4bb9fbf82b1c6cf00fc
Chicosk1 <168223635+Chicosk1@users.noreply.github.com>
2024-09-19 17:38:29 -0300

    Update Biblioteca.java

--- src/servicos/Biblioteca.java
+++ src/servicos/Biblioteca.java
@@ -5,16 +5,16 @@ import java.util.List;
 import modelos.*;
 
 public class Biblioteca {
+    
     private List<Livro> livros;
 
+    // Construtor
     public Biblioteca(List<Livro> livros) {
         this.livros = livros;
     }
 
+    // Métodos
     public void listarLivrosDoAutor(String autorListarEscolhido) {
-
-        
-        // Loop percorrendo os livros que um autor possui
         for (Livro livro : livros) {
             if (livro.getAutorLivro().getNomeAutor().equalsIgnoreCase(autorListarEscolhido)) {
                 System.out.println(livro.getTituloLivro());
@@ -24,8 +24,6 @@ public class Biblioteca {
     }
 
     public boolean livroExiste(String isbn, String titulo) {
-
-        // Loop verificando todos os livros do Array e validando se eles existem ou não
         for (Livro livro : livros) {
             if (livro.getIsbnLivro().equalsIgnoreCase(isbn)) {
                 System.out.println("Erro: Já existe um livro com este ISBN.");
